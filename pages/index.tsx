cf4edf1cc8017e6982a02220d182efbe0eaf6912
Aleksandr Mikhailov <iam@fidonode.me>
2024-01-04 02:20:43 +0100

    Add umami

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,17 @@ import PersonalCard from '../components/personal-card/PersonalCard';
 
 const Home = () => {
   return (
-    <div className="bg-gray-50 flex flex-col md:h-screen sm:h-full  justify-between">
-      <PersonalCard />
-      <Footer />
-    </div>
+    <>
+      <script
+        async
+        src="https://umami.dokutsu.xyz/script.js"
+        data-website-id="d52d9af1-0c7d-4531-84c6-0b9c2850011f"
+      ></script>
+      <div className="bg-gray-50 flex flex-col md:h-screen sm:h-full  justify-between">
+        <PersonalCard />
+        <Footer />
+      </div>
+    </>
   );
 };
 
